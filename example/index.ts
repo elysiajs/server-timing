03aa0951c5352af987cbf619048247292169343f
saltyaom <saltyaom@gmail.com>
2024-07-16 18:57:22 +0700

    :broom: chore: bump version

--- example/index.ts
+++ example/index.ts
@@ -3,22 +3,33 @@ import { serverTiming } from '../src/index'
 
 const delay = (time = 1000) => new Promise((r) => setTimeout(r, time))
 
-const app = new Elysia({ precompile: true })
+const app = new Elysia()
     .use(serverTiming())
-    .get('/', () => 'A', {
-        beforeHandle: [
-            async function a() {
-                await delay(100)
-                // return 'a'
-            }
-        ],
-        afterHandle: async () => {
-            await delay(20)
-        }
+    .onRequest(async function init() {
+        await delay(1)
     })
+    .get(
+        '/',
+        function demo() {
+            return 'Server Timing'
+        },
+        {
+            beforeHandle: [
+                async function a() {
+                    await delay(1)
+                },
+                async function b() {
+                    await delay(1)
+                }
+            ],
+            async afterHandle() {
+                await delay(1)
+            }
+        }
+    )
     .listen(3000)
 
-console.log(app.routes[0].composed?.toString())
+// console.log(app.routes[0].composed?.toString())
 
 const req = (path: string) => new Request(`http://localhost:8080${path}`)
 
--- src/index.ts
+++ src/index.ts
@@ -1,204 +1,218 @@
 import {
-    Elysia,
-    type Context,
-    type TraceEvent,
-    type TraceProcess
+	Elysia,
+	type Context,
+	type TraceEvent,
+	type TraceProcess
 } from 'elysia'
 
 type MaybePromise<T> = T | Promise<T>
 
 export interface ServerTimingOptions {
-    /**
-     * Should Elysia report data back to client via 'Server-Sent-Event'
-     */
-    report?: boolean
-    /**
-     * Allow Server Timing to log specified life-cycle events
-     */
-    trace?: {
-        /**
-         * Capture duration from request
-         *
-         * @default true
-         */
-        request?: boolean
-        /**
-         * Capture duration from parse
-         *
-         * @default true
-         */
-        parse?: boolean
-        /**
-         * Capture duration from transform
-         *
-         * @default true
-         */
-        transform?: boolean
-        /**
-         * Capture duration from beforeHandle
-         *
-         * @default true
-         */
-        beforeHandle?: boolean
-        /**
-         * Capture duration from handle
-         *
-         * @default true
-         */
-        handle?: boolean
-        /**
-         * Capture duration from afterHandle
-         *
-         * @default true
-         */
-        afterHandle?: boolean
-        /**
-         * Capture duration from mapResponse
-         *
-         * @default true
-         */
-        error?: boolean
-        /**
-         * Capture duration from mapResponse
-         *
-         * @default true
-         */
-        mapResponse?: boolean
-        /**
-         * Capture total duration from start to finish
-         *
-         * @default true
-         */
-        total?: boolean
-    }
-    /**
-     * Determine whether or not Server Timing should be enabled
-     *
-     * @default NODE_ENV !== 'production'
-     */
-    enabled?: boolean
-    /**
-     * A condition whether server timing should be log
-     *
-     * @default undefined
-     */
-    allow?:
-        | MaybePromise<boolean>
-        | ((context: Omit<Context, 'path'>) => MaybePromise<boolean>)
+	/**
+	 * Should Elysia report data back to client via 'Server-Sent-Event'
+	 */
+	report?: boolean
+	/**
+	 * Allow Server Timing to log specified life-cycle events
+	 */
+	trace?: {
+		/**
+		 * Capture duration from request
+		 *
+		 * @default true
+		 */
+		request?: boolean
+		/**
+		 * Capture duration from parse
+		 *
+		 * @default true
+		 */
+		parse?: boolean
+		/**
+		 * Capture duration from transform
+		 *
+		 * @default true
+		 */
+		transform?: boolean
+		/**
+		 * Capture duration from beforeHandle
+		 *
+		 * @default true
+		 */
+		beforeHandle?: boolean
+		/**
+		 * Capture duration from handle
+		 *
+		 * @default true
+		 */
+		handle?: boolean
+		/**
+		 * Capture duration from afterHandle
+		 *
+		 * @default true
+		 */
+		afterHandle?: boolean
+		/**
+		 * Capture duration from mapResponse
+		 *
+		 * @default true
+		 */
+		error?: boolean
+		/**
+		 * Capture duration from mapResponse
+		 *
+		 * @default true
+		 */
+		mapResponse?: boolean
+		/**
+		 * Capture total duration from start to finish
+		 *
+		 * @default true
+		 */
+		total?: boolean
+	}
+	/**
+	 * Determine whether or not Server Timing should be enabled
+	 *
+	 * @default NODE_ENV !== 'production'
+	 */
+	enabled?: boolean
+	/**
+	 * A condition whether server timing should be log
+	 *
+	 * @default undefined
+	 */
+	allow?:
+		| MaybePromise<boolean>
+		| ((context: Omit<Context, 'path'>) => MaybePromise<boolean>)
 }
 
 const getLabel = (
-    event: TraceEvent,
-    listener: (
-        callback: (process: TraceProcess<'begin', true>) => unknown
-    ) => unknown,
-    write: (value: string) => void
+	event: TraceEvent,
+	listener: (
+		callback: (process: TraceProcess<'begin', true>) => unknown
+	) => unknown,
+	write: (value: string) => void
 ) => {
-    listener(async ({ begin, onStop, onEvent, total }) => {
-        let label = ''
+	listener(async ({ onStop, onEvent, total }) => {
+		let label = ''
 
-        if (total === 0) return
+		if (total === 0) return
 
-        onEvent(({ name, index, onStop }) => {
-            onStop(({ elapsed }) => {
-                label += `${event}.${index}.${name};dur=${elapsed},`
-            })
-        })
+		onEvent(({ name, index, onStop }) => {
+			onStop(({ elapsed }) => {
+				label += `${event}.${index}.${name || 'anon'};dur=${elapsed},`
+			})
+		})
 
-        onStop(({ elapsed }) => {
-            label += `${event};dur=${elapsed},`
+		onStop(({ elapsed }) => {
+			label += `${event};dur=${elapsed},`
 
-            write(label)
-        })
-    })
+			write(label)
+		})
+	})
 }
 
 export const serverTiming = ({
-    allow,
-    enabled = process.env.NODE_ENV !== 'production',
-    trace: {
-        request: traceRequest = true,
-        parse: traceParse = true,
-        transform: traceTransform = true,
-        beforeHandle: traceBeforeHandle = true,
-        handle: traceHandle = true,
-        afterHandle: traceAfterHandle = true,
-        error: traceError = true,
-        mapResponse: traceMapResponse = true,
-        total: traceTotal = true
-    } = {}
+	allow,
+	enabled = process.env.NODE_ENV !== 'production',
+	trace: {
+		request: traceRequest = true,
+		parse: traceParse = true,
+		transform: traceTransform = true,
+		beforeHandle: traceBeforeHandle = true,
+		handle: traceHandle = true,
+		afterHandle: traceAfterHandle = true,
+		error: traceError = true,
+		mapResponse: traceMapResponse = true,
+		total: traceTotal = true
+	} = {}
 }: ServerTimingOptions = {}) => {
-    const app = new Elysia()
+	const app = new Elysia()
 
-    if (enabled) {
-        app.trace(
-            { as: 'global' },
-            async ({
-                onRequest,
-                onParse,
-                onTransform,
-                onBeforeHandle,
-                onHandle,
-                onAfterHandle,
-                onMapResponse,
-                onError,
-                set,
-                context,
-                context: {
-                    request: { method }
-                }
-            }) => {
-                let label = ''
+	if (enabled) {
+		app.trace(
+			{ as: 'global' },
+			async ({
+				onRequest,
+				onParse,
+				onTransform,
+				onBeforeHandle,
+				onHandle,
+				onAfterHandle,
+				onMapResponse,
+				onError,
+				set,
+				context,
+				context: {
+					request: { method }
+				}
+			}) => {
+				let label = ''
 
-                const write = (nextValue: string) => {
-                    label += nextValue
-                }
+				const write = (nextValue: string) => {
+					label += nextValue
+				}
 
-                if (traceRequest) getLabel('request', onRequest, write)
-                if (traceParse) getLabel('parse', onParse, write)
-                if (traceTransform) getLabel('transform', onTransform, write)
-                if (traceBeforeHandle)
-                    getLabel('beforeHandle', onBeforeHandle, write)
-                if (traceAfterHandle)
-                    getLabel('afterHandle', onAfterHandle, write)
-                if (traceError) getLabel('error', onError, write)
-                if (traceMapResponse)
-                    getLabel('mapResponse', onMapResponse, write)
+				let start: number
 
-                onHandle(({ name, onStop }) => {
-                    onStop(({ elapsed }) => {
-                        label += `handle.${name};dur=${elapsed}`
-                    })
-                })
+				onRequest(({ begin }) => {
+					start = begin
+				})
 
-                onMapResponse(({ onStop }) => {
-                    onStop(() => {
-                        set.headers['Server-Timing'] = label
-                    })
-                })
+				if (traceRequest) getLabel('request', onRequest, write)
+				if (traceParse) getLabel('parse', onParse, write)
+				if (traceTransform) getLabel('transform', onTransform, write)
+				if (traceBeforeHandle)
+					getLabel('beforeHandle', onBeforeHandle, write)
+				if (traceAfterHandle)
+					getLabel('afterHandle', onAfterHandle, write)
+				if (traceError) getLabel('error', onError, write)
+				if (traceMapResponse)
+					getLabel('mapResponse', onMapResponse, write)
 
-                let allowed = allow
-                if (allowed instanceof Promise) allowed = await allowed
+				if (traceHandle)
+					onHandle(({ name, onStop }) => {
+						onStop(({ elapsed }) => {
+							label += `handle.${name};dur=${elapsed}`
+						})
+					})
 
-                // ? Must wait until request is reported
-                switch (typeof allowed) {
-                    case 'boolean':
-                        if (allowed === false)
-                            delete set.headers['Server-Timing']
+				onMapResponse(({ onStop }) => {
+					onStop(async ({ end }) => {
+						let allowed = allow
+						if (allowed instanceof Promise) allowed = await allowed
 
-                        break
+						if (traceTotal) label += `total;dur=${end - start}`
 
-                    case 'function':
-                        if ((await allowed(context)) === false)
-                            delete set.headers['Server-Timing']
+						// ? Must wait until request is reported
+						switch (typeof allowed) {
+							case 'boolean':
+								if (allowed === false)
+									delete set.headers['Server-Timing']
 
-                        break
-                }
-            }
-        )
-    }
+								set.headers['Server-Timing'] = label
 
-    return app
+								break
+
+							case 'function':
+								if ((await allowed(context)) === false)
+									delete set.headers['Server-Timing']
+
+								set.headers['Server-Timing'] = label
+
+								break
+
+							default:
+								set.headers['Server-Timing'] = label
+						}
+					})
+				})
+			}
+		)
+	}
+
+	return app
 }
 
 export default serverTiming
